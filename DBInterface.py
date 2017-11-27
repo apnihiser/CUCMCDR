ad4d07ea125bdd5e6c5f3ef176cb96eba4e212d2
merkkie <apnihiser@gmail.com>
2017-11-27 14:59:55 -0500

    Update DBInterface.py

--- DBInterface.py
+++ DBInterface.py
@@ -4,13 +4,32 @@ import sqlite3
 import csv
 
 """
-DBInterface.py
+CUCMCDR.py
 
-Author:
+Author: A.Nihiser, apnihiser@gmail.com, Github: https://github.com/merkkie/CUCMCDR
 
-Purpose:
+Purpose: This entire script has been copy pasted into the new and improved CUCMCAR
+script. In a nutshell the script creates the required CDR & CMR databases automatically
+by pulling from a few text documents. These docs and the path to them may need to be 
+modified to fit in your environment depending on where your CMR, CDR documents are 
+being downloaded to, and version of Call Manager. I am running 10.5.2.12901-1.
 
-Issues:
+There is a pruning process that drops records after 60 days.
+
+All UNIX datestamps have been converted to a new easily readable format, and IPs
+are automatically converted as well.
+
+lastly all data is pulled and deleted from the chosen directory, in this case I am 
+using C:\dev\CUCM\IVC_CDR\CDR_Working_DIR. Once again this path may need to be changed
+depending on where your records are located.
+
+As of now we can view all CDR & CMR data by using SQLite3 databrowswer. Much more planned
+
+Future: Creating a Flask front end to interact and display data currently researching the
+best path to bring this alive in a smart and effective manner. I'd like to provided data
+searching, analytics and quality reports that can be searched by individual calls, daily,
+weekly, monthly time increments.
+
 """
 
 def main():
@@ -91,7 +110,7 @@ def db_purge(c):
 def convert_ips(signed32BitInt):
 
     if signed32BitInt == 0:
-        return "CDR returned 0 as IP"
+        return "0"
 
     hexValue = hex(signed32BitInt & (2**32-1))[2:10]
 
