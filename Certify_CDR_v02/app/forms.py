15e1493bd71a5ff4e3ab751c74660c3f7079304e
merkkie <apnihiser@gmail.com>
2018-01-19 13:38:57 -0500

    Delete forms.py

--- Certify_CDR_v02/app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_security.forms import LoginForm
-from wtforms import StringField
-from wtforms.validators import InputRequired
-
-class ExtendedLoginForm(LoginForm):
-    email = StringField('Username', [InputRequired()])
